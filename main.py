05d8cf1768e88daf448e08d23dca760f80af4e8a
EmirErden <103561700+EmirErden@users.noreply.github.com>
2022-05-18 22:18:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import cv2
 import numpy as np
+from PIL import Image
 
-img = cv2.imread("C:\\Users\\C605\\Desktop\\img.jpg")
-b, g, r = cv2.split(img)
+img = cv2.imread("C:\\Odev\\New_map.jpg", cv2.IMREAD_COLOR)
+r, b, g = cv2.split(img)
 cv2.imshow("Original image", img)
 cv2.imshow("Red", r)
 cv2.imshow("Blue", b)
 cv2.imshow("Green", g)
 
-diff_green = np.array([0, 130, 0])
-image_merge = cv2.merge([r, diff_green, b])
-cv2.imshow("different", image_merge)
-cv2.waitKey()
+im = Image.open("C:\\Odev\\New_map.jpg").convert("RGB")
+R, G, B = im.split()
+R = R.point(lambda i: i*0.5)
+result = Image.merge("RGB", (R, G, B))
+result.show()
+cv2.waitKey(0)
